4b32b73a0553cac47f7a8c76989f12774e8c0ede
priyanka1963 <55426801+priyanka1963@users.noreply.github.com>
2019-10-18 14:23:01 +0530

    user2update

--- IPC/FIFO/user2.c
+++ IPC/FIFO/user2.c
@@ -1,5 +1,4 @@
-// C program to implement one side of FIFO 
-// This side reads first, then reads 
+// first read then write
 #include <stdio.h> 
 #include <string.h> 
 #include <fcntl.h> 
@@ -11,27 +10,24 @@ int main()
 { 
 	int fd1; 
 
-	// FIFO file path 
+	
 	char * myfifo = "/tmp/myfifo"; 
 
-	// Creating the named file(FIFO) 
-	// mkfifo(<pathname>,<permission>) 
+	
 	mkfifo(myfifo, 0666); 
 
 	char str1[80], str2[80]; 
 	while (1) 
 	{ 
-		// First open in read only and read 
 		fd1 = open(myfifo,O_RDONLY); 
 		read(fd1, str1, 80); 
-
-		// Print the read string and close 
-		printf("User1: %s\n", str1); 
+ 
+		printf("priya: %s\n", str1); 
 		close(fd1); 
 
-		// Now open in write mode and write 
-		// string taken from user. 
+		
 		fd1 = open(myfifo,O_WRONLY); 
+		printf("Tanvi :- ");
 		fgets(str2, 80, stdin); 
 		write(fd1, str2, strlen(str2)+1); 
 		close(fd1); 
